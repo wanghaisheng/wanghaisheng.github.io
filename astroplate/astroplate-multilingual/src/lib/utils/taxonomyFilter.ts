b0b78ae125755e7ad920f75d0b54e09bdf52f83c
HeisenBerg? <edwin.uestc@gmail.com>
2024-12-09 20:45:01 +0800

    Delete astroplate directory

--- astroplate/astroplate-multilingual/src/lib/utils/taxonomyFilter.ts
+++
@@ -1,8 +0,0 @@
-import { slugify } from "@/lib/utils/textConverter";
-
-const taxonomyFilter = (posts: any[], name: string, key: string) =>
-  posts.filter((post) =>
-    post.data[name].map((name: string) => slugify(name)).includes(key),
-  );
-
-export default taxonomyFilter;
